aeb9735fbc868c1daa0b7a522a4323cdeeebf33d
Malleus Malegicarum <o@maleficarum.mx>
2025-07-24 14:41:14 -0600

    feat: Add init project

--- main.tf
+++ main.tf
@@ -1,34 +1,34 @@
 resource "oci_psql_db_system" "example_db" {
-  compartment_id      = var.compartment_id
-  display_name       = var.instance_configuration.name
-  shape              =   var.instance_configuration.shape
+  compartment_id = var.compartment_id
+  display_name   = var.instance_configuration.name
+  shape          = var.instance_configuration.shape
 
   storage_details {
     is_regionally_durable = var.instance_configuration.storage_is_regional
-    system_type          = var.instance_configuration.storage_type
+    system_type           = var.instance_configuration.storage_type
     #availability_domain  = data.oci_identity_availability_domains.ads.availability_domains[0].name
-    iops                = var.instance_configuration.iops
+    iops = var.instance_configuration.iops
   }
-  
-  db_version         = var.instance_configuration.version
+
+  db_version = var.instance_configuration.version
 
   credentials {
-    username         = "admin"     # Default admin username
+    username = "admin" # Default admin username
 
     password_details {
       password      = var.db_password
       password_type = "PLAIN_TEXT"
     }
   }
-  
+
   network_details {
-    subnet_id        = var.subnet
+    subnet_id = var.subnet
   }
 
   defined_tags = {
     "Oracle-Tags.CreatedBy"   = "default/terraform-cae",
     "Oracle-Tags.Environment" = var.environment
   }
-  
-  instance_count     = var.instance_configuration.instance_count
+
+  instance_count = var.instance_configuration.instance_count
 }
--- variables.tf
+++ variables.tf
@@ -1,20 +1,20 @@
 variable "instance_configuration" {
-    type = object({
-      name = string,
-      state = string,
-      version = string,#16
-      shape = string, #"PostgreSQL.VM.Standard.E4.2.32GB"
-      iops = number, #from 500 - 300 000,
-      storage_type = string #OCI_OPTIMIZED_STORAGE , LOCAL_STORAGE
-      storage_is_regional = bool, #false for development
-      instance_count = number
-    })
-    description = "Instance configuration"
+  type = object({
+    name                = string,
+    state               = string,
+    version             = string, #16
+    shape               = string, #"PostgreSQL.VM.Standard.E4.2.32GB"
+    iops                = number, #from 500 - 300 000,
+    storage_type        = string  #OCI_OPTIMIZED_STORAGE , LOCAL_STORAGE
+    storage_is_regional = bool,   #false for development
+    instance_count      = number
+  })
+  description = "Instance configuration"
 }
 
 variable "db_password" {
-    type = string
-    description = "The database password"
+  type        = string
+  description = "The database password"
 }
 
 
